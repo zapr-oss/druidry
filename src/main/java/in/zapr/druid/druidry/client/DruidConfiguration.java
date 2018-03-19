f9c6a5f72a707ea736388db089e5dabc874e87a9
Gagan Gupta <gagangupt16@gmail.com>
2018-03-19 21:27:23 +0530

    Added connection pooling for JerseyClient and valid checks for DruidConfiguration

--- src/main/java/in/zapr/druid/druidry/client/DruidConfiguration.java
+++ src/main/java/in/zapr/druid/druidry/client/DruidConfiguration.java
@@ -1,10 +1,11 @@
 package in.zapr.druid.druidry.client;
 
+import org.apache.commons.lang3.StringUtils;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Builder
 @Getter
 @Setter
 public class DruidConfiguration {
@@ -12,22 +13,58 @@ public class DruidConfiguration {
     private final int DEFAULT_HTTP_PORT = 80;
     private final int DEFAULT_HTTPS_PORT = 443;
 
-    private DruidQueryProtocol protocol = DruidQueryProtocol.HTTP;
+    private DruidQueryProtocol protocol;
     private String host;
     private Integer port;
     private String endpoint;
+    private Integer concurrentConnectionsRequired;
+
+    @Builder
+    private DruidConfiguration(DruidQueryProtocol protocol,
+                               String host,
+                               Integer port,
+                               String endpoint,
+                               Integer concurrentConnectionsRequired) {
+
+        if (StringUtils.isEmpty(host)) {
+            throw new IllegalArgumentException("Host cannot be null or empty");
+        }
+
+        if (port != null && port < 0) {
+            throw new IllegalArgumentException("Port cannot be negative");
+        }
+
+        if (concurrentConnectionsRequired != null && concurrentConnectionsRequired < 1) {
+            throw new IllegalArgumentException("Connections required cannot be less than 1");
+        }
+
+        if (protocol == null) {
+            protocol = DruidQueryProtocol.HTTP;
+        }
+
+        if (port == null) {
+            port = getDefaultPortOnBasisOfProtocol(protocol);
+        }
+
+        this.protocol = protocol;
+        this.host = host;
+        this.port = port;
+        this.endpoint = endpoint;
+        this.concurrentConnectionsRequired = concurrentConnectionsRequired;
+    }
 
     protected String getUrl() {
 
-        if (port == null) {
-            port = getDefaultPortOnBasisOfProtocol(this.protocol);
+        String endpoint = this.getEndpoint();
+        if (endpoint == null) {
+            endpoint = "";
         }
 
         return String.format("%s://%s:%d/%s",
                 this.getProtocol(),
                 this.getHost(),
                 this.getPort(),
-                this.getEndpoint());
+                endpoint);
     }
 
     private Integer getDefaultPortOnBasisOfProtocol(DruidQueryProtocol protocol) {
--- src/main/java/in/zapr/druid/druidry/client/DruidJerseyClient.java
+++ src/main/java/in/zapr/druid/druidry/client/DruidJerseyClient.java
@@ -1,10 +1,16 @@
 package in.zapr.druid.druidry.client;
 
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.glassfish.jersey.apache.connector.ApacheClientProperties;
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
+import org.glassfish.jersey.client.ClientConfig;
+
 import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,36 +18,73 @@ import javax.ws.rs.core.Response;
 import in.zapr.druid.druidry.client.exception.ConnectionException;
 import in.zapr.druid.druidry.client.exception.QueryException;
 import in.zapr.druid.druidry.query.DruidQuery;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class DruidJerseyClient implements DruidClient {
 
-    private final Client client;
-    private final String url;
+    private static final int DEFAULT_CONNECTION_POOL_LIMIT = 5;
+    private final String druidUrl;
+    private final DruidConfiguration druidConfiguration;
 
-    public DruidJerseyClient(DruidConfiguration druidConfiguration) {
+    private ClientConfig jerseyConfig;
+    private Client client;
+    private WebTarget queryWebTarget;
 
-        this.client = ClientBuilder.newClient();
-        this.url = druidConfiguration.getUrl();
-        log.debug("Will query to {}", url);
+    public DruidJerseyClient(@NonNull DruidConfiguration druidConfiguration,
+                             ClientConfig jerseyConfig) {
+
+        this.druidUrl = druidConfiguration.getUrl();
+        this.jerseyConfig = jerseyConfig;
+
+        log.debug("Will query to {}", druidUrl);
+        this.druidConfiguration = druidConfiguration;
     }
 
     @Override
     public void connect() throws ConnectionException {
 
+        PoolingHttpClientConnectionManager connectionManager
+                = new PoolingHttpClientConnectionManager();
+
+        int numberOfConnectionsInPool = DEFAULT_CONNECTION_POOL_LIMIT;
+
+        if (this.druidConfiguration.getConcurrentConnectionsRequired() != null) {
+            numberOfConnectionsInPool = this.druidConfiguration.getConcurrentConnectionsRequired();
+        }
+
+        connectionManager.setDefaultMaxPerRoute(numberOfConnectionsInPool);
+
+        try {
+            if (jerseyConfig == null) {
+                this.jerseyConfig = new ClientConfig();
+            }
+
+            this.jerseyConfig.property(ApacheClientProperties.CONNECTION_MANAGER, connectionManager);
+            this.jerseyConfig.connectorProvider(new ApacheConnectorProvider());
+
+            this.client = ClientBuilder.newClient(this.jerseyConfig);
+            this.queryWebTarget = this.client.target(this.druidUrl);
+
+        } catch (Exception e) {
+            throw new ConnectionException(e);
+        }
     }
 
     @Override
     public void close() throws ConnectionException {
-        this.client.close();
+        try {
+            this.client.close();
+        } catch (Exception e) {
+            throw new ConnectionException(e);
+        }
     }
 
     @Override
     public String query(DruidQuery druidQuery) throws QueryException {
 
-        try (Response response = this.client
-                .target(this.url)
+        try (Response response = this.queryWebTarget
                 .request(MediaType.APPLICATION_JSON)
                 .post(Entity.entity(druidQuery, MediaType.APPLICATION_JSON))) {
 
@@ -52,14 +95,14 @@ public class DruidJerseyClient implements DruidClient {
             return response.readEntity(String.class);
 
         } catch (Exception e) {
+            log.error("Exception while querying {}", e);
             throw new QueryException(e);
         }
     }
 
     @Override
     public <T> List<T> query(DruidQuery druidQuery, Class<T> className) throws QueryException {
-        try (Response response = this.client
-                .target(this.url)
+        try (Response response = this.queryWebTarget
                 .request(MediaType.APPLICATION_JSON)
                 .post(Entity.entity(druidQuery, MediaType.APPLICATION_JSON))) {
 
