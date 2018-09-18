6f79b2f473aec2d4d9ddcc4fe1eba855f64ba48b
Gagan Gupta <gagangupt16@gmail.com>
2018-09-18 18:07:01 +0530

    #44 EqualsAndHashCode for SearchSortSpec

--- src/main/java/in/zapr/druid/druidry/query/search/SearchSortSpec.java
+++ src/main/java/in/zapr/druid/druidry/query/search/SearchSortSpec.java
@@ -1,9 +1,11 @@
 package in.zapr.druid.druidry.query.search;
 
 import in.zapr.druid.druidry.SortingOrder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 public class SearchSortSpec {
 
     private SortingOrder type;
