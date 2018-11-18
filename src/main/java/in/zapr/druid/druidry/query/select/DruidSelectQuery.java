21ddd7aea3eb6ff378afcc0b27a509a5880c7eaa
dmitry <d.stepanov@zozowfm.com>
2018-11-18 21:09:24 +0500

    add license

--- src/main/java/in/zapr/druid/druidry/query/select/DruidSelectQuery.java
+++ src/main/java/in/zapr/druid/druidry/query/select/DruidSelectQuery.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018-present Red Brick Lane Marketing Solutions Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package in.zapr.druid.druidry.query.select;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -36,7 +51,7 @@ public class DruidSelectQuery extends DruidQuery {
             List<DruidDimension> dimensions,
             List<String> metrics,
             @NonNull List<Interval> intervals,
-            PagingSpec pagingSpec,
+            @NonNull PagingSpec pagingSpec,
             Context context) {
         this.queryType = QueryType.SELECT;
         this.context = context;
